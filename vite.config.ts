a12dff14e0a311b72a008cb6805ac273f16b2d52
User <user@example.com>
2026-07-03 23:34:21 +0800

    v3.3: fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cycle-tracker/',
   build: { outDir: 'docs' },
   plugins: [react()],
   server: {
